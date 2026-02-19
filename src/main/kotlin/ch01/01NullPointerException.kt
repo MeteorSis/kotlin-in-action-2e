bc256bb3ccf0185542c3de45fa507240352f3ab1
Craig <craig@daangn.com>
2026-02-19 22:46:17 +0900

    init project

---
+++ src/main/kotlin/ch01/01NullPointerException.kt
@@ -0,0 +1,15 @@
+package ch01
+
+fun main() {
+    val s: String? = null
+    val s2: String = ""
+
+	//println(s.length) // compile error
+    println(s?.length)
+    try {
+    	println(s!!.length)
+    } catch (e: NullPointerException) {
+        println(e)
+    }
+	println(s2.length)
+}
