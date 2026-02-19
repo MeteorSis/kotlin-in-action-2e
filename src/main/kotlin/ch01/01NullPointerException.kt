40014919625c541e1d73fe63c22f2cf5d89e4cd0
Craig <craig@daangn.com>
2026-02-19 22:50:24 +0900

    fix project

--- src/main/kotlin/ch01/01NullPointerException.kt
+++ src/main/kotlin/ch01/01NullPointerException.kt
@@ -4,7 +4,7 @@ fun main() {
     val s: String? = null
     val s2: String = ""
 
-	//println(s.length) // compile error
+    //println(s.length) // compile error
     println(s?.length)
     try {
     	println(s!!.length)
