46afe8179d6cfe297d006320799356c51a49b7a7
Craig <craig@daangn.com>
2026-02-23 23:01:53 +0900

    ch04: delegation

---
+++ src/main/kotlin/ch04/01Delegation.kt
@@ -0,0 +1,43 @@
+package ch04
+
+class CountingSet<T> : HashSet<T>() {
+    var objectsAdded = 0
+        private set
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return super.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectsAdded += elements.size
+        return super.addAll(elements)
+    }
+}
+
+class CountingSetByDelegation<T>(
+    private val innerSet: MutableCollection<T> = HashSet<T>()
+) : MutableCollection<T> by innerSet {
+    var objectsAdded = 0
+        private set
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectsAdded += elements.size
+        return innerSet.addAll(elements)
+    }
+}
+
+fun main() {
+    val cset = CountingSet<Int>()
+    cset.addAll(listOf(1, 1, 2))
+    println("CountingSet: Added ${cset.objectsAdded} objects, ${cset.size} uniques.")
+
+    val csetByDelegation = CountingSetByDelegation<Int>()
+    csetByDelegation.addAll(listOf(1, 1, 2))
+    println("CountingSetByDelegation: Added ${csetByDelegation.objectsAdded} objects, ${csetByDelegation.size} uniques.")
+}
